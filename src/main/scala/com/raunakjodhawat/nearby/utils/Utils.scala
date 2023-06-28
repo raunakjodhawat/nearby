a57087a5cad9bed00345791b051ed45006a251e7
Raunak Jodhawat <raunakjodhawat@gmail.com>
2023-06-28 15:17:58 +0530

    Revert "Revert "add user activation logic""
    This reverts commit f36f187a298338ff2aa5023e103a1aa99d4c5753.

--- src/main/scala/com/raunakjodhawat/nearby/utils/Utils.scala
+++ src/main/scala/com/raunakjodhawat/nearby/utils/Utils.scala
@@ -1,7 +1,17 @@
 package com.raunakjodhawat.nearby.utils
 
+import courier._
+
+import scala.util.{Failure, Properties, Success}
+import scala.concurrent.ExecutionContext.Implicits.global
 import java.util.UUID.randomUUID
+import javax.mail.internet.InternetAddress
 object Utils {
+  val mailerHost = Properties.envOrElse("mailerHost", "")
+  val mailerUsername = Properties.envOrElse("mailerUsername", "")
+  val mailerPassword = Properties.envOrElse("mailerPassword", "")
+  val env = Properties.envOrElse("env", "")
+
   // credits: https://stackoverflow.com/a/32445372
   private val emailRegex =
     """^[a-zA-Z0-9\.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$""".r
@@ -17,4 +27,35 @@ object Utils {
   def isValidUsername(username: String): Boolean = username.length > 5 && username.length < 32
   def isValidPassword(password: String): Boolean = password.length > 5 && password.length < 32
 
+  def createUrl(secret: String): String = env match {
+    case "development" => s"http://localhost:8080/api/v1/verify/$secret"
+  }
+  def sendEmail(secret: String, username: String, receiverEmail: String) = {
+    val url = createUrl(secret)
+    val content =
+      s"""
+         |Hello $username,
+         |
+         |Please click $url to verify your email address.
+         |If the above link does not work, please copy and paste the following $url in your browser.
+         |
+         |Thank you,
+         |Nearby Team
+         |""".stripMargin
+
+    val mailer = Mailer(mailerHost, 587)
+      .auth(true)
+      .as(mailerUsername, mailerPassword)
+      .startTls(true)()
+    mailer(
+      Envelope
+        .from(new InternetAddress(mailerUsername))
+        .to(new InternetAddress(receiverEmail))
+        .subject("Nearby, verify your email")
+        .content(Text(content))
+    )(global).onComplete {
+      case Success(_) => println("message delivered")
+      case Failure(e) => println(s"delivery failed, ${e.getMessage}")
+    }(global)
+  }
 }
