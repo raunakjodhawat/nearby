fc4c09aa7ef7cd746c65f9676fb712d8d0ae039f
Raunak Jodhawat <raunakjodhawat@gmail.com>
2023-07-26 13:21:17 +0530

    create DB, and initialize with ZIO

--- src/main/scala/com/raunakjodhawat/nearby/Application.scala
+++ src/main/scala/com/raunakjodhawat/nearby/Application.scala
@@ -3,25 +3,36 @@ package com.raunakjodhawat.nearby
 import com.raunakjodhawat.nearby.controllers.Controller
 import com.raunakjodhawat.nearby.models.user.UsersTable
 
-import scala.util.{Failure, Success}
 import zio.http._
 import zio._
 import slick.jdbc.PostgresProfile.api._
 
-import scala.concurrent.ExecutionContext.Implicits.global
 object Application extends ZIOAppDefault {
+  private def initializeDB: ZIO[Any, Throwable, Database] = (for {
+    db <- ZIO.attempt(Database.forConfig("postgres"))
+    updateFork <- ZIO.fromFuture { ex =>
+      {
+        db.run(
+          DBIO.seq(
+            TableQuery[UsersTable].schema.dropIfExists,
+            TableQuery[UsersTable].schema.createIfNotExists
+          )
+        )
+      }
+    }.fork
+    dbUpdateResult <- updateFork.await
+  } yield dbUpdateResult match {
+    case Exit.Success(_) => ZIO.succeed(println("Database Initialization complete")) *> ZIO.from(db)
+    case Exit.Failure(cause) =>
+      ZIO.succeed(println(s"Database Initialization errored, ${cause.failures}")) *> ZIO.fail(
+        new Exception("Failed to initialize")
+      )
+  }).flatMap(x => x)
+
   val db = Database.forConfig("postgres")
-  db.run(
-    DBIO.seq(
-      TableQuery[UsersTable].schema.dropIfExists,
-      TableQuery[UsersTable].schema.createIfNotExists
-    )
-  ).onComplete {
-    case Success(_)         => println("Database Initialization complete")
-    case Failure(exception) => println(exception.getMessage)
-  }
+
   private val base_path: Path = Root / "api" / "v1"
 
   private val app = Controller(base_path, db).mapError(e => Response.fromHttpError(HttpError.BadRequest(e.getMessage)))
-  override def run = Server.serve(app).provide(Server.default)
+  override def run = initializeDB *> Server.serve(app).provide(Server.default)
 }
