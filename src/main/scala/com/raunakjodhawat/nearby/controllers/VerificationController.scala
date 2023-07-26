498383256534e45d24dd72ef003bb3c498cc8657
Raunak Jodhawat <raunakjodhawat@gmail.com>
2023-07-26 16:19:04 +0530

    update verification print statement

--- src/main/scala/com/raunakjodhawat/nearby/controllers/VerificationController.scala
+++ src/main/scala/com/raunakjodhawat/nearby/controllers/VerificationController.scala
@@ -7,19 +7,16 @@ import zio._
 import zio.http._
 
 class VerificationController(userRepository: UserRepository) {
-  def verifyUser(id: Long, secret_key: String): ZIO[Database, Throwable, Response] = {
-    println("verify user")
-    for {
-      resultZIO <- for {
-        fib <- userRepository
-          .verifyUser(id, secret_key)
-          .fork
-        result <- fib.await
-      } yield result match {
-        case Exit.Success(_)     => ZIO.succeed(s"User with $id verified")
-        case Exit.Failure(cause) => ZIO.failCause(cause)
-      }
-      response <- resultZIO
-    } yield Response.text(response)
-  }
+  def verifyUser(id: Long, secret_key: String): ZIO[Database, Throwable, Response] = for {
+    resultZIO <- for {
+      fib <- userRepository
+        .verifyUser(id, secret_key)
+        .fork
+      result <- fib.await
+    } yield result match {
+      case Exit.Success(_)     => ZIO.succeed(s"User with $id verified")
+      case Exit.Failure(cause) => ZIO.failCause(cause)
+    }
+    response <- resultZIO
+  } yield Response.text(response)
 }
