1123274b6bfdcd9e243709bb1e5ab29d4b661539
Raunak Jodhawat <raunakjodhawat@gmail.com>
2023-07-26 14:31:51 +0530

    update verification controller

--- src/main/scala/com/raunakjodhawat/nearby/controllers/VerificationController.scala
+++ src/main/scala/com/raunakjodhawat/nearby/controllers/VerificationController.scala
@@ -1,35 +1,27 @@
 package com.raunakjodhawat.nearby.controllers
 
 import com.raunakjodhawat.nearby.repository.user.UserRepository
-import com.raunakjodhawat.nearby.models.user.JsonEncoderDecoder._
+import slick.jdbc.PostgresProfile.api._
 
 import zio._
 import zio.http._
-import zio.json._
 
-import scala.concurrent.ExecutionContext
-
-class VerificationController(base_path: Path, userRepository: UserRepository)(implicit
-  ec: ExecutionContext
-) {
+class VerificationController(base_path: Path, userRepository: UserRepository) {
   private val api_path = base_path / "verify"
-  val verify_api_route = Http.collectZIO[Request] {
-    case Method.GET -> api_path / long(id) =>
-      verifyUser(1, "secret_key")
-    case _ => {
-      println("reachint here")
-      ZIO.succeed(Response.text("Invalid request"))
-    }
+  val verify_api_route = Http.collectZIO[Request] { case Method.GET -> api_path / long(id) / secret_key =>
+    verifyUser(id, secret_key)
   }
 
-  private def verifyUser(id: Long, secret_key: String): ZIO[Any, Throwable, Response] = {
-    println("verify user")
-    userRepository
-      .verifyUser(id, secret_key)
-      .join
-      .map {
-        case Some(user) => Response.json(user.toJson)
-        case None       => Response.status(Status.NotFound)
-      }
-  }
+  def verifyUser(id: Long, secret_key: String): ZIO[Database, Throwable, Response] = for {
+    resultZIO <- for {
+      fib <- userRepository
+        .verifyUser(id, secret_key)
+        .fork
+      result <- fib.await
+    } yield result match {
+      case Exit.Success(_)     => ZIO.succeed(s"User with $id verified")
+      case Exit.Failure(cause) => ZIO.failCause(cause)
+    }
+    response <- resultZIO
+  } yield Response.text(response)
 }
