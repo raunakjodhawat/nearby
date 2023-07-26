5b190348b124a9ed09c33ffc6d43e4c2a2e3d344
Raunak Jodhawat <raunakjodhawat@gmail.com>
2023-07-26 14:48:14 +0530

    update user controller

--- src/main/scala/com/raunakjodhawat/nearby/controllers/UserController.scala
+++ src/main/scala/com/raunakjodhawat/nearby/controllers/UserController.scala
@@ -5,6 +5,8 @@ import com.raunakjodhawat.nearby.utils.Utils.sendEmail
 import com.raunakjodhawat.nearby.models.user.JsonEncoderDecoder._
 import com.raunakjodhawat.nearby.models.user.User
 
+import slick.jdbc.PostgresProfile.api._
+
 import zio._
 import zio.http._
 import zio.json._
@@ -21,59 +23,80 @@ class UserController(base_path: Path, userRepository: UserRepository)(implicit
     case req @ Method.POST -> api_path / long(id) =>
       updateUser(req.body, id)
     case Method.GET -> api_path =>
-      getAllUsers()
+      getAllUsers
     case req @ Method.POST -> api_path =>
       createUser(req.body)
   }
 
-  private def getAllUsers(): ZIO[Any, Throwable, Response] = {
-    println("get all users")
-    userRepository
-      .getAllUsers()
-      .join
-      .map(x => Response.json(x.toJson))
-  }
+  private def getAllUsers: ZIO[Database, Throwable, Response] = for {
+    resultZIO <- for {
+      fib <- userRepository.getAllUsers.fork
+      res <- fib.await
+    } yield res match {
+      case Exit.Success(v)     => ZIO.succeed(v)
+      case Exit.Failure(cause) => ZIO.failCause(cause)
+    }
+    result <- resultZIO
+  } yield Response.json(result.toJson)
 
-  private def getUserById(id: Long): ZIO[Any, Throwable, Response] = {
-    userRepository
-      .getUserById(id)
-      .join
-      .map {
-        case Some(user) => Response.json(user.toJson)
-        case None       => Response.status(Status.NotFound)
-      }
-  }
+  private def getUserById(id: Long): ZIO[Database, Throwable, Response] = for {
+    resultZIO <- for {
+      fib <- userRepository.getUserById(id).fork
+      res <- fib.await
+    } yield res match {
+      case Exit.Success(v)     => ZIO.succeed(v)
+      case Exit.Failure(cause) => ZIO.failCause(cause)
+    }
+    result <- resultZIO
+  } yield Response.json(result.toJson)
 
   // Used for signup requests
-  private def createUser(body: Body): ZIO[Any, Throwable, Response] = {
+  private def createUser(body: Body): ZIO[Database, Throwable, Response] = {
     body.asString
       .map(_.fromJson[User])
       .flatMap {
         case Left(e) => ZIO.fail(new Exception(e))
         case Right(user) =>
-          userRepository
-            .createUser(user)
-            .join
-            .map { user =>
-              sendEmail(user.secret.get, user.username, user.email)
-              Response.status(Status.Created)
+          for {
+            resultZIO <- for {
+              fib <- userRepository
+                .createUser(user)
+                .fork
+              res <- fib.await
+            } yield res match {
+              case Exit.Success(v)     => ZIO.succeed(v)
+              case Exit.Failure(cause) => ZIO.failCause(cause)
             }
+            user <- resultZIO
+          } yield {
+            sendEmail(user.secret.get, user.username, user.email)
+            Response.json(user.toJson)
+          }
       }
   }
 
-  private def updateUser(body: Body, id: Long): ZIO[Any, Throwable, Response] = {
+  private def updateUser(body: Body, id: Long): ZIO[Database, Throwable, Response] = {
     body.asString
       .map(_.fromJson[User])
       .flatMap {
         case Left(e) => ZIO.fail(new Exception(e))
-        case Right(user) =>
-          userRepository
-            .updateUser(user, id)
-            .join
-            .map {
-              case Some(user) => Response.json(user.toJson)
-              case None       => Response.status(Status.NotFound)
+        case Right(user) => {
+          for {
+            resultZIO <- for {
+              fib <- userRepository
+                .updateUser(user, id)
+                .fork
+              res <- fib.await
+            } yield res match {
+              case Exit.Success(v)     => ZIO.succeed(v)
+              case Exit.Failure(cause) => ZIO.failCause(cause)
             }
+            user <- resultZIO
+          } yield {
+            sendEmail(user.secret.get, user.username, user.email)
+            Response.json(user.toJson)
+          }
+        }
       }
   }
 }
