86f80ab26336b1261dc38c2b5aebbb9714542199
Raunak Jodhawat <raunakjodhawat@gmail.com>
2023-07-29 11:16:41 +0530

    tests for utils

--- src/main/scala/com/raunakjodhawat/nearby/utils/Utils.scala
+++ src/main/scala/com/raunakjodhawat/nearby/utils/Utils.scala
@@ -4,8 +4,9 @@ import courier._
 
 import scala.util.Properties
 import scala.concurrent.ExecutionContext.Implicits.global
-import java.util.UUID.randomUUID
+
 import javax.mail.internet.InternetAddress
+
 import zio._
 import zio.Random
 
@@ -32,45 +33,48 @@ object Utils {
   def isValidPassword(password: String): Boolean = password.length > 5 && password.length < 32
 
   def createUrl(id: Long, secret: String): String = env match {
-    case "development" => s"http://localhost:8080/api/v1/verify/$id/$secret"
+    case "development" | "test" => s"http://localhost:8080/api/v1/verify/$id/$secret"
   }
-  def sendEmail(secret: String, id: Long, username: String, receiverEmail: String): ZIO[Any, Throwable, Unit] = {
-    val url = createUrl(id, secret)
-    val content =
-      s"""
-         |Hello $username,
-         |
-         |Please click $url to verify your email address.
-         |If the above link does not work, please copy and paste the following $url in your browser.
-         |
-         |Thank you,
-         |Nearby Team
-         |""".stripMargin
 
-    val mailerZIO: ZIO[Any, Throwable, Mailer] = ZIO.attempt(
-      Mailer(mailerHost, 587)
-        .auth(true)
-        .as(mailerUsername, mailerPassword)
-        .startTls(true)()
-    )
+  def createEmailContent(username: String, url: String): String =
+    s"""
+       |Hello $username,
+       |
+       |Please click $url to verify your email address.
+       |If the above link does not work, please copy and paste the following $url in your browser.
+       |
+       |Thank you,
+       |Nearby Team
+       |""".stripMargin
 
-    for {
-      mailer <- mailerZIO
-      mailSenderFiber <- ZIO.fromFuture { ex =>
-        {
-          mailer(
-            Envelope
-              .from(new InternetAddress(mailerUsername))
-              .to(new InternetAddress(receiverEmail))
-              .subject("Nearby, verify your email")
-              .content(Text(content))
-          )
-        }
-      }.fork
-      mailSenderResult <- mailSenderFiber.await
-    } yield mailSenderResult match {
-      case Exit.Success(_) => println("message delivered")
-      case Exit.Failure(_) => println(s"delivery failed")
-    }
+  def mailerZIO: ZIO[Any, Throwable, Mailer] = ZIO.attempt(
+    Mailer(mailerHost, 587)
+      .auth(true)
+      .as(mailerUsername, mailerPassword)
+      .startTls(true)()
+  )
+  def sendEmail(
+    secret: String,
+    id: Long,
+    username: String,
+    receiverEmail: String,
+    zio: ZIO[Any, Throwable, Mailer] = mailerZIO
+  ): ZIO[Any, Throwable, Unit] = for {
+    mailer <- zio
+    mailSenderFiber <- ZIO.fromFuture { ex =>
+      {
+        mailer(
+          Envelope
+            .from(new InternetAddress(mailerUsername))
+            .to(new InternetAddress(receiverEmail))
+            .subject("Nearby, verify your email")
+            .content(Text(createEmailContent(username, createUrl(id, secret))))
+        )
+      }
+    }.fork
+    mailSenderResult <- mailSenderFiber.await
+  } yield mailSenderResult match {
+    case Exit.Success(_) => println("message delivered")
+    case Exit.Failure(_) => println(s"delivery failed")
   }
 }
