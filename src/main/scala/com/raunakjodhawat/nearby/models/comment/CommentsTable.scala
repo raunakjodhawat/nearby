927a690e1c2ae4a926b96425885f77ae5cd9c99f
Raunak Jodhawat <raunakjodhawat@gmail.com>
2023-09-08 13:42:43 +0530

    friendship table

--- src/main/scala/com/raunakjodhawat/nearby/models/comment/CommentsTable.scala
+++ src/main/scala/com/raunakjodhawat/nearby/models/comment/CommentsTable.scala
@@ -8,26 +8,21 @@ import slick.ast.TypedType
 import slick.jdbc.PostgresProfile.api.*
 import slick.lifted.{ProvenShape, Tag}
 
-import scala.reflect.ClassTag
-
 object CommentsTable {
-  implicit val postContentTypedType: TypedType[PostContent] = MappedColumnType.base[PostContent, String](
+  given postContentTypedType: TypedType[PostContent] = MappedColumnType.base[PostContent, String](
     e => e.toString,
     str => {
       val parts = str.split(",")
       PostContent(parts(0), if (parts.length > 1) Some(parts(1)) else None)
     }
   )
-  implicit val dateMapping: TypedType[Date] = MappedColumnType.base[Date, String](
-    e => e.toString,
-    _ => new Date()
-  )
 }
 
 val users = TableQuery[UsersTable]
 val posts = TableQuery[PostsTable]
 class CommentsTable(tag: Tag) extends Table[Comment](tag, "COMMENTS") {
-  import CommentsTable._
+  import com.raunakjodhawat.nearby.models.common.typedtypes.Mappings.dateMapping
+  import CommentsTable.postContentTypedType
   def id = column[Long]("ID", O.PrimaryKey, O.AutoInc)
   def post_id = column[Long]("POSTID")
   def post_id_fk = foreignKey("POST_ID_FK", post_id, posts)(_.id,
